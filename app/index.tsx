199c282dbfa3862f9ff4c3694811cf0e3b23517b
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2026-04-16 17:37:44 +0530

    started, working tailwind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-[#0e0e0e]">
+      <Text className="text-blue-400 text-3xl font-extrabold">I knew it this will work!!</Text>
+    </View>
+  );
+}
